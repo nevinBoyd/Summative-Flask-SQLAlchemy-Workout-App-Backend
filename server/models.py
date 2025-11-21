cfb1ebb1c1ceed3ff0d56857c3f2a8d1662b914b
Nevin <n.boyd1@yahoo.com>
2025-11-21 12:00:55 -0800

    Add relationships between models

--- server/models.py
+++ server/models.py
@@ -10,6 +10,8 @@ class Exercise(db.Model):
     category = db.Column(db.String)
     equipment_needed = db.Column(db.Boolean, default=False)
 
+    workout_exercises = db.relationship('WorkoutExercise', backref='exercise', cascade="all, delete")
+    workouts = db.relationship('Workout', secondary='workout_exercises', back_populates='exercises')
 
 class Workout(db.Model):
     __tablename__ = 'workouts'
@@ -19,12 +21,13 @@ class Workout(db.Model):
     duration_minutes = db.Column(db.Integer)
     notes = db.Column(db.Text)
 
+    workout_exercises = db.relationship('WorkoutExercise', backref='workout', cascade="all, delete")
+    exercises = db.relationship('Exercise', secondary='workout_exercises', back_populates='workouts')
 
 class WorkoutExercise(db.Model):
     __tablename__ = 'workout_exercises'
 
     id = db.Column(db.Integer, primary_key=True)
-    
     workout_id = db.Column(db.Integer, db.ForeignKey('workouts.id'), nullable=False)
     exercise_id = db.Column(db.Integer, db.ForeignKey('exercises.id'), nullable=False)
 
